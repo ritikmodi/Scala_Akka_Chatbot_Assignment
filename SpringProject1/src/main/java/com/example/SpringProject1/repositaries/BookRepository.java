09281d0ad9a7a1348279f00efa15e75c3c9c710c
Ritik Modi <r.modi@adform.com>
2024-06-27 00:04:05 +0530

    added sbt

--- SpringProject1/src/main/java/com/example/SpringProject1/repositaries/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringProject1.repositaries;
-
-import com.example.SpringProject1.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
